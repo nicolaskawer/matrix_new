fc14e3f632e27141ee9e2d53b7f9af64e4d1cf6f
hadarlevy <94686791+hadarlevy@users.noreply.github.com>
2022-03-22 13:16:42 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,92 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def make_elementary_matrix(pivot, num1, row, col):
+    temp_elementary = make_unit_matrix()
+    temp_elementary[row][col] = -1 * (num1 / pivot)
+    return temp_elementary
+
+
+def exchange(row, row_replace):
+    unit_matrix = temp_elementary = make_unit_matrix()
+    temp_elementary[row], temp_elementary[row_replace] = unit_matrix[row_replace], unit_matrix[row]
+    return temp_elementary
+
+
+def gauss_method(matrix):
+    mul = make_unit_matrix()
+    """open a file"""
+    for r in range(size):
+        maxi = abs(matrix[r][r])
+        """1111"""
+        flag = 0
+        for c in range(size):
+            if abs(matrix[r][c]) > maxi:
+                maxi = abs(matrix[r][c])
+                flag = 1
+                c_max = c
+        if flag != 0:
+            temp_matrix = exchange(r, c_max)
+            mul = multiply_two_matrix(temp_matrix, mul)
+            """print the multiply to a file"""
+            matrix = multiply_two_matrix(temp_matrix, matrix)
+    for r in range(size):
+        for c in range(r, size):
+            if matrix[r][r] == 1:
+                temp_matrix = make_elementary_matrix(matrix[r][r], matrix[c][r], c, r)
+                mul = multiply_two_matrix(temp_matrix, mul)
+                """print the multiply to a file"""
+                matrix = multiply_two_matrix(temp_matrix, matrix)
+            else:
+                temp_matrix = make_unit_matrix()
+                if matrix[r][r] < 0:
+                    temp_matrix[r][r] = -1 / matrix[r][r]
+                else:
+                    temp_matrix[r][r] = 1 / matrix[r][r]
+                mul = multiply_two_matrix(temp_matrix, mul)
+                """print the multiply to a file"""
+                matrix = multiply_two_matrix(temp_matrix, matrix)
+    return matrix
+
+
+def multiply_two_matrix(matrix1, matrix2):
+    result = []
+    for r in range(len(matrix1)):
+        helper_res = []
+        for c in range(len(matrix1)):
+            helper_res.append(0)
+        result.append(helper_res)
+    for i in range(len(matrix1)):
+        # iterating by column by B
+        for j in range(len(matrix2[0])):
+            # iterating by rows of B
+            for k in range(len(matrix2)):
+                result[i][j] += matrix1[i][k] * matrix2[k][j]
+    return result
+
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    matrix = []
+    answer_matrix = []
+    size = int(input("Enter size of the matrix n >> (nXn): "))
+    for i in range(size):
+        helper = []
+        for j in range(size):
+            num = float(input(f'matrix[{i}][{j}]='))
+            helper.append(num)
+        matrix.append(helper)
+        num = float(input(f'result num in row {i + 1}='))
+        answer_matrix.append(num)
+print(gauss_method(matrix))
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
